505c71bc2314b8b0e8049110fb96b28c9dad83b1
娄松 <>
2025-01-14 10:51:13 +0800

    feat: config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,10 +28,9 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh',
+    locales: ['zh'],
   },
-
   presets: [
     [
       'classic',
